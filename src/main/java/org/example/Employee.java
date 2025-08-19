0f5012528116625b2a35b7a8683e4a9e53c877b1
Sumit kumar <sumitkumar808393@gmail.com>
2025-08-19 22:03:34 +0530

    Db query

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -4,7 +4,7 @@ public class Employee {
     public static void main (String [] args){
         mysqlConn Operations = new mysqlConn();
         Operations.createDatabase("TestingDb");
-        Operations.createTable("TestingDb", "Employee_Detail");
+        Operations.createTable("TestinggDb", "Employee_Detail");
     }
 }
 
